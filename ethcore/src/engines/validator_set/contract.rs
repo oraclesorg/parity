4f6c9722906d578a0caa108697c42f95d619e9d8
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-01-09 13:19:36 +0000

    Revert "Merge pull request #55 from poanetwork/vk-report-malicious-immutable"
    This reverts commit f83dc39cce5d566e260307da11c208ca3968282b, reversing
    changes made to 1323ef7668bac6ba2be0e772d254f721de2fb138.

--- ethcore/src/engines/validator_set/contract.rs
+++ ethcore/src/engines/validator_set/contract.rs
@@ -32,8 +32,7 @@ use byteorder::{ByteOrder, LittleEndian};
 use keccak_hasher::KeccakHasher;
 
 use hashdb::Hasher;
-use ethkey::Signature;
-use error::Error;
+use ethkey::{sign, Signature};
 
 use super::{ValidatorSet, SimpleList, SystemCall};
 use super::safe_contract::ValidatorSafeContract;
@@ -79,7 +78,7 @@ impl ValidatorSet for ValidatorContract {
 		self.validators.default_caller(id)
 	}
 
-	fn on_epoch_begin(&self, first: bool, header: &Header, call: &mut SystemCall) -> Result<(), Error> {
+	fn on_epoch_begin(&self, first: bool, header: &Header, call: &mut SystemCall) -> Result<(), ::error::Error> {
 		self.validators.on_epoch_begin(first, header, call)
 	}
 
@@ -100,7 +99,7 @@ impl ValidatorSet for ValidatorContract {
 		self.validators.signals_epoch_end(first, header, aux)
 	}
 
-	fn epoch_set(&self, first: bool, machine: &EthereumMachine, number: BlockNumber, proof: &[u8]) -> Result<(SimpleList, Option<H256>), Error> {
+	fn epoch_set(&self, first: bool, machine: &EthereumMachine, number: BlockNumber, proof: &[u8]) -> Result<(SimpleList, Option<H256>), ::error::Error> {
 		self.validators.epoch_set(first, machine, number, proof)
 	}
 
@@ -116,13 +115,7 @@ impl ValidatorSet for ValidatorContract {
 		self.validators.count_with_caller(bh, caller)
 	}
 
-	fn report_malicious(
-		&self,
-		address: &Address,
-		_set_block: BlockNumber,
-		block: BlockNumber,
-		signer: &dyn Fn(H256) -> Result<Signature, Error>
-	) -> Result<(), Error> {
+	fn report_malicious(&self, address: &Address, _set_block: BlockNumber, block: BlockNumber, signer: &mut dyn FnMut(H256) -> Signature) {
 		let message = {
 			let mut buf = vec![0; 52];
 			address.copy_to(&mut buf[..20]);
@@ -131,17 +124,17 @@ impl ValidatorSet for ValidatorContract {
 		};
 
 		let signature = {
-			let signature = signer(KeccakHasher::hash(&message))?;
+			let signature = signer(KeccakHasher::hash(&message));
 			let mut v = Vec::with_capacity(signature.len());
 			v.extend_from_slice(signature.deref());
 			v
 		};
 
 		let data = validator_report::functions::report_malicious_validator::encode_input(message, signature);
-		Ok(match self.transact(data) {
+		match self.transact(data) {
 			Ok(_) => warn!(target: "engine", "Reported malicious validator {}", address),
 			Err(s) => warn!(target: "engine", "Validator {} could not be reported {}", address, s),
-		})
+		}
 	}
 
 	fn report_benign(&self, address: &Address, _set_block: BlockNumber, block: BlockNumber) {
--- ethcore/src/engines/validator_set/mod.rs
+++ ethcore/src/engines/validator_set/mod.rs
@@ -43,7 +43,6 @@ use self::safe_contract::ValidatorSafeContract;
 use self::multi::Multi;
 use super::SystemCall;
 use ethkey::Signature;
-use error::Error;
 
 /// Creates a validator set from spec.
 pub fn new_validator_set(spec: ValidatorSpec) -> Box<ValidatorSet> {
@@ -90,7 +89,7 @@ pub trait ValidatorSet: Send + Sync + 'static {
 	/// The caller provided here may not generate proofs.
 	///
 	/// `first` is true if this is the first block in the set.
-	fn on_epoch_begin(&self, _first: bool, _header: &Header, _call: &mut SystemCall) -> Result<(), Error> {
+	fn on_epoch_begin(&self, _first: bool, _header: &Header, _call: &mut SystemCall) -> Result<(), ::error::Error> {
 		Ok(())
 	}
 
@@ -126,7 +125,7 @@ pub trait ValidatorSet: Send + Sync + 'static {
 	/// Returns the set, along with a flag indicating whether finality of a specific
 	/// hash should be proven.
 	fn epoch_set(&self, first: bool, machine: &EthereumMachine, number: BlockNumber, proof: &[u8])
-		-> Result<(SimpleList, Option<H256>), Error>;
+		-> Result<(SimpleList, Option<H256>), ::error::Error>;
 
 	/// Checks if a given address is a validator, with the given function
 	/// for executing synchronous calls to contracts.
@@ -139,15 +138,7 @@ pub trait ValidatorSet: Send + Sync + 'static {
 	fn count_with_caller(&self, parent_block_hash: &H256, caller: &Call) -> usize;
 
 	/// Notifies about malicious behaviour.
-	fn report_malicious(
-		&self,
-		_validator: &Address,
-		_set_block: BlockNumber,
-		_block: BlockNumber,
-		_signer: &dyn Fn(H256) -> Result<Signature, Error>
-	) -> Result<(), Error> {
-		Ok(())
-	}
+	fn report_malicious(&self, _validator: &Address, _set_block: BlockNumber, _block: BlockNumber, _signer: &mut dyn FnMut(H256) -> Signature) {}
 	/// Notifies about benign misbehaviour.
 	fn report_benign(&self, _validator: &Address, _set_block: BlockNumber, _block: BlockNumber) {}
 	/// Allows blockchain state access.
