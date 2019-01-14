8e17c763683e71528933c1815ae4a8a06f6952a6
Andreas Fackler <AndreasFackler@gmx.de>
2019-01-14 15:29:01 +0100

    Remove consensus_kind from AuthorityRound.

--- ethcore/src/engines/authority_round/mod.rs
+++ ethcore/src/engines/authority_round/mod.rs
@@ -31,7 +31,7 @@ use engines::{Engine, Seal, EngineError, ConstructedVerifier};
 use engines::block_reward;
 use engines::block_reward::{BlockRewardContract, RewardKind};
 use error::{Error, ErrorKind, BlockError};
-use ethjson::{self, spec::authority_round::ConsensusKind};
+use ethjson;
 use machine::{AuxiliaryData, Call, EthereumMachine};
 use hash::keccak;
 use super::signer::EngineSigner;
@@ -86,8 +86,6 @@ pub struct AuthorityRoundParams {
 	pub maximum_empty_steps: usize,
 	/// Transition block to strict empty steps validation.
 	pub strict_empty_steps_transition: u64,
-	/// Sets whether Aura will use Proof of Authority (PoA) or Proof of Stake (PoS) consensus.
-	pub consensus_kind: ConsensusKind,
 	/// If set, enables random number contract integration.
 	pub randomness_contract_address: Option<Address>,
 }
@@ -120,7 +118,6 @@ impl From<ethjson::spec::AuthorityRoundParams> for AuthorityRoundParams {
 			empty_steps_transition: p.empty_steps_transition.map_or(u64::max_value(), |n| ::std::cmp::max(n.into(), 1)),
 			maximum_empty_steps: p.maximum_empty_steps.map_or(0, Into::into),
 			strict_empty_steps_transition: p.strict_empty_steps_transition.map_or(0, Into::into),
-			consensus_kind: p.consensus_kind.unwrap_or(ConsensusKind::Poa),
 			randomness_contract_address: p.randomness_contract_address.map(Into::into),
 		}
 	}
@@ -435,7 +432,6 @@ pub struct AuthorityRound {
 	empty_steps_transition: u64,
 	strict_empty_steps_transition: u64,
 	maximum_empty_steps: usize,
-	consensus_kind: ConsensusKind,
 	machine: EthereumMachine,
 	/// The stored secret contribution to randomness.
 	// TODO: Only used in PoS. Maybe make part of `ConsensusKind`? Or tie together with `randomness_contract_address`?
@@ -694,7 +690,6 @@ impl AuthorityRound {
 				empty_steps_transition: our_params.empty_steps_transition,
 				maximum_empty_steps: our_params.maximum_empty_steps,
 				strict_empty_steps_transition: our_params.strict_empty_steps_transition,
-				consensus_kind: our_params.consensus_kind,
 				machine: machine,
 				rand_secret: Default::default(),
 				randomness_contract_address: our_params.randomness_contract_address,
@@ -1587,7 +1582,7 @@ mod tests {
 	use engines::{Seal, Engine, EngineError, EthEngine};
 	use engines::validator_set::{TestSet, SimpleList};
 	use error::{Error, ErrorKind};
-	use super::{AuthorityRoundParams, AuthorityRound, EmptyStep, SealedEmptyStep, calculate_score, ConsensusKind};
+	use super::{AuthorityRoundParams, AuthorityRound, EmptyStep, SealedEmptyStep, calculate_score};
 
 	fn aura<F>(f: F) -> Arc<AuthorityRound> where
 		F: FnOnce(&mut AuthorityRoundParams),
@@ -1607,7 +1602,6 @@ mod tests {
 			block_reward_contract_transition: 0,
 			block_reward_contract: Default::default(),
 			strict_empty_steps_transition: 0,
-			consensus_kind: ConsensusKind::Poa,
 			randomness_contract_address: None,
 		};
 
