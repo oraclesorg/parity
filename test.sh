vgtom/parity-ethereum

Fix quoting in test.sh
`$@` should always be in double-quotes.

test.sh
@@ -35,9 +35,9 @@ validate () {
   if [ "$VALIDATE" -eq "1" ]
   then
     echo "________Validate build________"
-    time cargo check $@ --frozen --no-default-features
-    time cargo check $@ --frozen --manifest-path util/io/Cargo.toml --no-default-features
-    time cargo check $@ --frozen --manifest-path util/io/Cargo.toml --features "mio"
+    time cargo check "$@" --frozen --no-default-features
+    time cargo check "$@" --frozen --manifest-path util/io/Cargo.toml --no-default-features
+    time cargo check "$@" --frozen --manifest-path util/io/Cargo.toml --features "mio"
 
     # Validate chainspecs
     echo "________Validate chainspecs________"
@@ -71,7 +71,7 @@ cpp_test () {
 cargo_test () {
   echo "________Running Parity Full Test Suite________"
   git submodule update --init --recursive
-  time cargo test $OPTIONS --features "$FEATURES" --frozen --all $@ -- --test-threads $THREADS
+  time cargo test $OPTIONS --features "$FEATURES" --frozen --all "$@" -- --test-threads $THREADS
 }
 
 
@@ -89,12 +89,12 @@ then
 
   case "${RUN_TESTS}" in
     (cargo|all)
-      cargo_test --target $CARGO_TARGET $@
+      cargo_test --target $CARGO_TARGET "$@"
       ;;
     ('')
-      cargo_test --no-run --target $CARGO_TARGET $@
+      cargo_test --no-run --target $CARGO_TARGET "$@"
       ;;
   esac
 else
-  cargo_test $@
+  cargo_test "$@"
 fi

oraclesorg/parity

test.sh
@@ -14,9 +14,9 @@ validate () {
   if [ "$VALIDATE" -eq "1" ]
   then
     echo "________Validate build________"
-    time cargo check $@ --locked --no-default-features
-    time cargo check $@ --locked --manifest-path util/io/Cargo.toml --no-default-features
-    time cargo check $@ --locked --manifest-path util/io/Cargo.toml --features "mio"
+    time cargo check "$@" --locked --no-default-features
+    time cargo check "$@" --locked --manifest-path util/io/Cargo.toml --no-default-features
+    time cargo check "$@" --locked --manifest-path util/io/Cargo.toml --features "mio"
 
     # Validate chainspecs
     echo "________Validate chainspecs________"
@@ -50,7 +50,7 @@ cpp_test () {
 cargo_test () {
   echo "________Running Parity Full Test Suite________"
   git submodule update --init --recursive
-  time cargo test $OPTIONS --features "$FEATURES" --locked --all $@ -- --test-threads $THREADS
+  time cargo test $OPTIONS --features "$FEATURES" --locked --all "$@" -- --test-threads $THREADS
 }
 
 
@@ -68,12 +68,12 @@ then
 
   case "${RUN_TESTS}" in
     (cargo|all)
-      cargo_test --target $CARGO_TARGET $@
+      cargo_test --target $CARGO_TARGET "$@"
       ;;
     ('')
-      cargo_test --no-run --target $CARGO_TARGET $@
+      cargo_test --no-run --target $CARGO_TARGET "$@"
       ;;
   esac
 else
-  cargo_test $@
+  cargo_test "$@"
 fi