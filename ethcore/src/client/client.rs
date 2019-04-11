8475f7600f79d0e46f6721e0e0665ad26be495bd
davidf <dforsten@gmail.com>
2019-04-12 00:04:34 +0200

    Implemented register_client for the hbbft Consensus Engine
    Set up a test with hbbft Engine, Client, Miner and TestNotify

--- ethcore/src/client/client.rs
+++ ethcore/src/client/client.rs
@@ -996,7 +996,7 @@ impl Client {
 	}
 
 	/// Get shared miner reference.
-	#[cfg(test)]
+	#[cfg(any(test, feature = "test-helpers"))]
 	pub fn miner(&self) -> Arc<Miner> {
 		self.importer.miner.clone()
 	}
