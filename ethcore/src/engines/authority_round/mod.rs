6965ceeb72b8c3e7d16f5ffd19bfa12a4dccd573
Andreas Fackler <afck@users.noreply.github.com>
2019-05-30 13:04:16 +0200

    Queue malice reports and fix the unit tests. (#129)
    This adds the malice report queue: Reports are included whenever we create a block ourselves, and they are also being resent as regular transactions until they have been successfully included in a block.
    
    reportMalicious now matches the ABI used by the validator set code. It also checks whether the removed validator exists.
    
    Squashed commits:
    ```
    * Update the test validator set contract.
    
    `reportMalicious` now matches the ABI used by the validator set code.
    It also checks whether the removed validator exists.
    
    * Add maliceReportedForBlock to test contract.
    
    * Add back benign report check.
    
    * Fix reportBenign ABI.
    
    * Explicitly check malice report in the test.
    
    * Queue malice reports
    
    This is a squash of 18 commits:
    
    Queue failed reports for later insertion into blocks
    
    If we fail to send a transaction that reports a validator as malicious,
    store it on a stack.  When we next seal a block, included the failed
    transactions.
    
    Insert our reports every time we prepare a block
    
    Implement queued reporting
    
    Prioritize the `emitInitiateChange` call
    
    Fix warnings
    
    These warnings cluttered build output, and made it hard to see actual
    warnings in new code.
    
    Implement (partial) cache purging.
    
    Once an entry that cannot be purged is found, further entries are
    skipped.
    
    Finish report queuing
    
    Use the correct reporting address
    
    Drop reports beyond MAX_QUEUED_REPORTS
    
    This helps prevent denial-of-service attacks.
    
    Drop queued report limits
    
    We now only keep 500 reports that have not been confirmed, or 100
    reports that have been.
    
    Lower limits on queued reports
    
    Add a script to generate ABI files
    
    and use it to fix compilation error due to incomplete ABI json.
    
    Remove old validator reports
    
    Reports over 100 blocks old are useless anyway.
    
    Fix an unused variable warning in the test client
    
    Show a better error when the validator set contract is bad
    
    Fix backwards comparison between bad and current block numbers
    
    Drop reports for block numbers ahead of the current one
    
    Fix review comments from Andreas
    
    Remove a commented-out constant, and use the standard library
    `VecDeque::retain()` method instead of (inefficiently) reimplementing it
    ourselves.
    
    * Queue reports even if transaction creation was successful.
    
    Having created a transaction doesn't guarantee that it will be
    included in a block. We should queue the transaction anyway.
    
    Also remove some unnecessary closures and match statements.
    
    * Fix malice report nonce addr; disable transactions.
    
    The nonce for malice reports needs to be the engine signer's, not the contract's.
    Creating malice report transactions makes the `reports_validators` test hang,
    so it is disabled for now.
    
    * Make the unit tests work again.
    
    * Retry sending queued malice reports.
    
    * Fix malice report block numbers.
    
    * added a constant to limit resending of returned reports
    
    * skip at least one block when reporting malicious validators
    
    * restored instantaneous malicious reports
    
    * Filter malice reports more aggressively.
    
    * Update test contracts: add isValidatorBanned.
    ```

--- ethcore/src/engines/authority_round/mod.rs
+++ ethcore/src/engines/authority_round/mod.rs
@@ -961,6 +961,7 @@ impl Engine<EthereumMachine> for AuthorityRound {
 		header_expected_seal_fields(header, self.empty_steps_transition)
 	}
 
+	#[cfg(test)]
 	fn step(&self) {
 		self.step.inner.increment();
 		self.step.can_propose.store(true, AtomicOrdering::SeqCst);
@@ -1295,7 +1296,12 @@ impl Engine<EthereumMachine> for AuthorityRound {
 			},
 		};
 
-		block_reward::apply_block_rewards(&rewards, block, &self.machine)
+		block_reward::apply_block_rewards(&rewards, block, &self.machine)?;
+
+		match self.signer.read().as_ref() {
+			Some(signer) => self.validators.on_close_block(block.header(), &signer.address()),
+			None => Ok(()), // We are not a validator, so we can't report malicious validators.
+		}
 	}
 
 	/// Make calls to the randomness and validator set contracts.
