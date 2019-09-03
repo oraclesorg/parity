2a53bcc267ee53e79321d5e7bb967ef1a6b7df63
Andreas Fackler <AndreasFackler@gmx.de>
2019-09-03 18:09:22 +0200

    Make cargo test --all work.

--- ethcore/src/engines/authority_round/mod.rs
+++ ethcore/src/engines/authority_round/mod.rs
@@ -1048,7 +1048,6 @@ impl Engine<EthereumMachine> for AuthorityRound {
 		header_expected_seal_fields(header, self.empty_steps_transition)
 	}
 
-	#[cfg(test)]
 	fn step(&self) {
 		self.step.inner.increment();
 		self.step.can_propose.store(true, AtomicOrdering::SeqCst);
